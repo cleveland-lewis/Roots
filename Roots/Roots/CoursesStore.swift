9556e3f5741e77173ce0adb60b86411685efc6ca
cleveland-lewis <lewisclevelandiii@gmail.com>
2025-12-01 00:31:14 -0500

    CoursesStore: add Semester model and current semester filtering

--- Roots/Roots/CoursesStore.swift
+++ Roots/Roots/CoursesStore.swift
@@ -1,6 +1,11 @@
 import SwiftUI
 import Combine
 
+struct Semester: Identifiable, Hashable {
+    let id: UUID
+    var name: String
+}
+
 struct Course: Identifiable, Hashable {
     let id: UUID
     var name: String
@@ -10,11 +15,29 @@ struct Course: Identifiable, Hashable {
     var color: Color
     var credits: Int
     var isArchived: Bool
+    var semesterId: UUID?
 }
 
 final class CoursesStore: ObservableObject {
     @Published var courses: [Course] = []
+    @Published var semesters: [Semester] = []
+    @Published var currentSemesterId: UUID? = nil
 
+    var currentSemesterCourses: [Course] {
+        guard let sid = currentSemesterId else { return [] }
+        return courses.filter { $0.semesterId == sid && !$0.isArchived }
+    }
+
+    // Semester management
+    func addSemester(_ semester: Semester) {
+        semesters.append(semester)
+    }
+
+    func setCurrentSemester(_ id: UUID?) {
+        currentSemesterId = id
+    }
+
+    // Course CRUD
     func add(_ course: Course) {
         courses.append(course)
     }
