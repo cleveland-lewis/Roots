c438a6d20150d38de86d5a3f083fc1af193468f4
cleveland-lewis <lewisclevelandiii@gmail.com>
2025-11-30 20:58:38 -0500

    Initial Commit

---
+++ Roots/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Roots
+//
+//  Created by Cleveland Lewis III on 11/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RootsTests/RootsTests.swift
@@ -0,0 +1,16 @@
+//
+//  RootsTests.swift
+//  RootsTests
+//
+//  Created by Cleveland Lewis III on 11/30/25.
+//
+
+import Testing
+
+struct RootsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
